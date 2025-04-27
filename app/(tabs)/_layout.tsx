b1fe3bf1b5bed68165bf831173d7d62413041ea4
olathegreat <ademolarotimi2000@gmail.com>
2025-04-27 19:56:53 +0100

    feat

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
-import { Link, Tabs } from 'expo-router';
+import { Entypo, FontAwesome } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
 
-import { HeaderButton } from '../../components/HeaderButton';
 import { TabBarIcon } from '../../components/TabBarIcon';
 
 export default function TabLayout() {
@@ -12,20 +12,54 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <HeaderButton />
-            </Link>
-          ),
+          title: 'Home screen',
+          tabBarIcon: ({ color, size }) => <Entypo name="home" color={color} size={size} />,
+          tabBarShowLabel: false,
+          headerShown: false,
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="search"
         options={{
           title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color }) => <TabBarIcon name="search" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: 'Create',
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarIconStyle: {
+            backgroundColor: '#f1f5f9',
+            borderRadius: 16,
+            width: 48,
+            marginTop: 4,
+          },
+          tabBarIcon: ({ color }) => <TabBarIcon name="plus" color={color} />,
+        }}
+      />
+
+      <Tabs.Screen
+        name="notifications"
+        options={{
+          title: 'Notifications',
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarIcon: ({ color }) => <TabBarIcon name="heart" color={color} />,
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarIcon: ({ color ,size}) => <TabBarIcon name="user"  color={color} />  
         }}
       />
     </Tabs>
